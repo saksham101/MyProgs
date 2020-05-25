6ecedb23f8c5ab5b9bffb7944e0ceef8770526f5
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-05-25 16:42:26 +0530

    Add files via upload

---
+++ search.py
@@ -0,0 +1,8 @@
+ele = list()
+el = input("enter the elements")
+ele=ele.append(el)
+search= input("enter the element you want to search")
+for el in ele :
+  if el == search :
+    print("exist")
+  
