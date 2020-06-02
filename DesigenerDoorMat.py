2e4c756ecfc368f3e2ed735fb830e1ec785fe451
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-06-02 15:21:44 +0530

    Add files via upload

---
+++ DesigenerDoorMat.py
@@ -0,0 +1,62 @@
+"""
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number,N and M is  3 times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+Input Format
+
+A single line containing the space separated values of N and M.
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+
+9 27
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+"""
+# SOLUTION
+def doorMat(N,M) :
+    s = ".|."
+    for i in range(N//2) :
+        print((s*(2*i+1)).center(M,"-"))
+    print("WELCOME".center(M,"-"))
+    for i in reversed(range(N//2)) :
+        print((s*(2*i+1)).center(M,"-"))
+i = input().split()
+doorMat(int(i[0]),int(i[1]))
