474050e320613d465331c62b8215b8d41bc2ab21
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-06-04 16:20:50 +0530

    Create PatternHR.c

---
+++ PatternHR.c
@@ -0,0 +1,84 @@
+/*
+In this problem, you need to print the pattern of the following form containing the numbers from 1 to n.
+
+                            4 4 4 4 4 4 4  
+                            4 3 3 3 3 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 2 1 2 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 3 3 3 3 4   
+                            4 4 4 4 4 4 4   
+Input Format
+
+The input will contain a single integer n.
+
+
+Output Format
+
+Print the pattern mentioned in the problem statement.
+
+Sample Input 0
+
+2
+Sample Output 0
+
+2 2 2
+2 1 2
+2 2 2
+Sample Input 1
+
+5
+Sample Output 1
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+Sample Input 2
+
+7
+Sample Output 2
+
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 2 1 2 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+*/
+//SOLUTION
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n, m;
+    scanf("%d", &n);
+    m = 2*n-1;
+  	// Complete the code to print the pattern.
+      for(int i = 0;i < m ; i++)
+      {
+          for(int j = 0;j < m ; j ++)
+          {
+              int min = i < j ? i : j ;
+              min = min < m-i ? min : m-i-1;
+              min = min < m-j-1 ? min : m-j-1;
+              printf("%d ",n-min);
+          }
+          printf("\n");
+      }
+    return 0;
+}
