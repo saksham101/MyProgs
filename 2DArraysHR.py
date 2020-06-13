db456980fcf6e56c42242c62feb85996a8193875
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-06-13 09:33:55 +0530

    Create 2DArraysHR.py

---
+++ 2DArraysHR.py
@@ -0,0 +1,90 @@
+"""
+Objective
+Today, we're building on our knowledge of Arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video!
+
+Context
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers describing 2D Array ; every value in  will be in the inclusive range of  to .
+
+Constraints
+
+Output Format
+
+Print the largest (maximum) hourglass sum found in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+
+"""
+# SOLUTION
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+arr = []
+for _ in range(6):
+    arr.append(list(map(int, input().rstrip().split())))
+height , width = 6 , 6
+high_score = None
+for a in range(height - 2):
+    for b in range(width - 2):
+        total = sum(arr[a][b:b+3]) + arr[a+1][b+1] + sum(arr[a+2][b:b+3])
+        if high_score is None or total > high_score:
+            high_score = total  
+print(high_score)
