ba73215653ab70febe625a1af9a45bed0c55e092
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-06-16 16:24:28 +0530

    Create SherlockSndSquareHr.py

---
+++ SherlockSndSquareHr.py
@@ -0,0 +1,82 @@
+"""
+Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value describing a range of integers. Sherlock must determine the number of square integers within that range, inclusive of the endpoints.
+
+Note: A square integer is an integer which is the square of an integer, e.g. .
+
+For example, the range is  and , inclusive. There are three square integers in the range:  and .
+
+Function Description
+
+Complete the squares function in the editor below. It should return an integer representing the number of square integers in the inclusive range from  to .
+
+squares has the following parameter(s):
+
+a: an integer, the lower range boundary
+b: an integer, the uppere range boundary
+Input Format
+
+The first line contains , the number of test cases.
+Each of the next  lines contains two space-separated integers denoting  and , the starting and ending integers in the ranges.
+
+Constraints
+
+
+
+Output Format
+
+For each test case, print the number of square integers in the range on a new line.
+
+Sample Input
+
+2
+3 9
+17 24
+Sample Output
+
+2
+0
+Explanation
+
+Test Case #00: In range ,  and  are the two square integers.
+Test Case #01: In range , there are no square integers.
+
+
+"""
+# SOLUTION
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the squares function below.
+def squares(a, b):
+    count = 0
+    while(a <= b) :
+        root = math.sqrt(a)
+        if str(math.sqrt(a))[-2:] == ".0":
+            count += 1
+            a = (a//(math.sqrt(a))+1)**2
+        else:
+            a += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        ab = input().split()
+
+        a = int(ab[0])
+
+        b = int(ab[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
