8d8fa6103ce3e4fc18e21ea07f6a0432553df627
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-07-02 14:52:02 +0530

    Create SolveMeFirstHR.cpp

---
+++ SolveMeFirstHR.cpp
@@ -0,0 +1,44 @@
+/*
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+
+The sum of the two integers  and  is computed as: .
+*/
+// SOLUTION
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  return a+b;
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
