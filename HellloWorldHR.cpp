79a4463d21ca25a92b7418d91decd384de5411ff
Saksham Garg <55405810+sakshusakshusakshu@users.noreply.github.com>
2020-07-02 14:54:38 +0530

    Create HellloWorldHR.cpp

---
+++ HellloWorldHR.cpp
@@ -0,0 +1,24 @@
+/*
+We're starting out by printing the most famous computing phrase of all time! In the editor below, use either printf or cout to print the string  to stdout.
+
+Input Format
+
+You do not need to read any input in this challenge.
+
+Output Format
+
+Print  to stdout.
+
+Sample Output
+
+Hello, World!
+*/
+//SOLUTION
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
