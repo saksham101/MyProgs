a0f3f0b66d8f5e4b074dbf9ae05c6741f6a82f6e
Saksham Garg <gargsaksham101@gmail.com>
2021-06-11 14:05:01 +0530

    Update 2DArraysHR.py

--- 2DArraysHR.py
+++ 2DArraysHR.py
@@ -25,8 +25,6 @@ Input Format
 
 There are  lines of input, where each line contains  space-separated integers describing 2D Array ; every value in  will be in the inclusive range of  to .
 
-Constraints
-
 Output Format
 
 Print the largest (maximum) hourglass sum found in .
